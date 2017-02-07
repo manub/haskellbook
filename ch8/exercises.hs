c9150a225c64ce7aa44319d7db7cf2865c51f8b0
Emanuele Blanco <emanuele.blanco@gmail.com>
2017-02-07 21:59:43 +0000

    Finished chapter 8

--- ch8/exercises.hs
+++ ch8/exercises.hs
@@ -14,7 +14,23 @@ module Chapter8 where
     | b < -1 = multRec (a+a) (b+1)
     | otherwise = multRec (a+a) (b-1)
 
+  data DividedResult =
+      Result (Integer, Integer) 
+    | DividedByZero deriving Show
 
+  dividedBy :: Integer -> Integer -> DividedResult 
+  dividedBy _ 0 = DividedByZero
+  dividedBy num den = go (abs num) (abs den) 0
+    where go n d count 
+            | n < d && num > 0 && den > 0 = Result (count, n)
+            | n < d && num < 0 && den > 0 = Result (negate count, negate n)
+            | n < d && num < 0 && den < 0 = Result (count, negate n)
+            | n < d && num > 0 && den < 0 = Result (negate count, n)
+            | otherwise = go (n - d) d (count + 1)
 
-  
+  mc91 :: (Num a, Ord a) => a -> a
+  mc91 n 
+    | n > 100   = n - 10
+    | otherwise = mc91 $ mc91 $ n + 11    
+    
 
